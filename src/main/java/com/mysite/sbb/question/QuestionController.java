e104402f77bddcb2c02bcf0071f12695425b1527
kms152000 <kms152000@naver.com>
2025-08-28 11:53:00 +0900

    23강

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -3,7 +3,6 @@ package com.mysite.sbb.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.stream.Collectors;
 
@@ -14,7 +13,6 @@ public class QuestionController {
     private final QuestionRepository questionRepository;
 
     @GetMapping("/question/list")
-    @ResponseBody
     public String list() {
 
         String questions = questionRepository.findAll()
@@ -23,14 +21,6 @@ public class QuestionController {
                 .collect(Collectors.joining("\n"));
 
 
-        return """
-                <html>
-                    <body>
-                        <ul>
-                          %s
-                        </ul>
-                    </body>
-                </html>
-                """.formatted(questions);
+        return "question_list";
     }
 }
